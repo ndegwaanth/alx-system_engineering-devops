a702499241e06c851aba7bf63fda6765d521ee34
Anthony <ndegwaanthony300@gmail.com>
2024-03-07 01:02:09 +0300

    test the server

---
+++ demo/start.js
@@ -0,0 +1,15 @@
+function run_code() {
+    var color = new_color();
+    color[0] = "red"
+    console.color
+    console.alert("are you sure");
+}
+
+function click() {
+
+    console.confirm("are those names real?");
+}
+
+function just() { 
+    console.prompt("enter your name", "Anthony");
+}
