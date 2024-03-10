be867dd35d22100f5804b452e3792f9ef48d8572
Anthony <ndegwaanthony300@gmail.com>
2024-03-10 22:55:17 +0000

    initial commit

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,47 +1,30 @@
-# Define class to configure custom HTTP response header
-class nginx_custom_header {
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-  # Install Nginx package
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  # Create custom 404 error page
-  file { '/usr/share/nginx/html/custom_404.html':
-    ensure  => present,
-    content => '404 - Not Found',
-  }
-
-  # Add custom response header to Nginx configuration
-  file_line { 'custom_http_header':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default',
-    line    => '  add_header X-Served-By $hostname;',
-    require => Package['nginx'],
-  }
-
-  # Configure Nginx to use custom 404 error page
-  file_line { 'custom_404_error_page':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default',
-    line    => '  error_page 404 /custom_404.html;',
-    require => File['/usr/share/nginx/html/custom_404.html'],
-  }
-
-  # Restart Nginx service to apply changes
-  exec { 'nginx_reload':
-    command     => 'systemctl restart nginx',
-    refreshonly => true,
-    subscribe   => [File_line['custom_http_header'], File_line['custom_404_error_page']],
-  }
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
 
-# Apply the class to all nodes
-include nginx_custom_header
+package { 'nginx':
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
