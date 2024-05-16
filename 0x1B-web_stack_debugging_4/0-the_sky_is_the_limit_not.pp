c9c4e1735e83a2d0b556a973aee557f53d41b9ff
Anthony <ndegwaanthony300@gmail.com>
2024-05-16 21:27:41 +0300

    file one modified

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,43 +1,11 @@
-# File: nginx_setup.pp
+# fix request dropping in nginx
 
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure => installed,
+exec { 'increase ulimit':
+  path    => '/bin',
+  command => "sed -i 's/15/2000/g' /etc/default/nginx"
 }
 
-# Define Nginx configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => present,
-  content => template('nginx/nginx.conf.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Define Nginx service
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
-
-# Executing AB command to test before and after configuration
-exec { 'ab_before':
-  command     => '/usr/bin/ab -c 100 -n 2000 localhost/',
-  logoutput   => true,
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/nginx.conf'],
-}
-
-# Reload Nginx service after configuration changes
-exec { 'nginx_reload':
-  command     => '/usr/sbin/nginx -s reload',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/nginx.conf'],
-}
-
-# Executing AB command to test after configuration changes
-exec { 'ab_after':
-  command     => '/usr/bin/ab -c 100 -n 2000 localhost/',
-  logoutput   => true,
-  refreshonly => true,
-  require     => Exec['nginx_reload'],
+exec { 'nginx restart':
+  path    => '/usr/sbin',
+  command => 'service nginx restart'
 }
